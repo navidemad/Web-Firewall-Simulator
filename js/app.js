2ffb2ba7cc92d92763f46e8a723cf3b7e5b9a845
Antoine Lamy <antoinelamy@Antoines-MBP.csusm.edu>
2016-06-22 11:35:58 -0700

    Add rule

--- js/app.js
+++ js/app.js
@@ -1,5 +1,27 @@
 var module = angular.module('myApp', []);
 
 module.controller('MainController', function($scope) {
+
+  var generateEmptyRule = function () {
+    return { // Used for adding rules feature
+      ruleName: '',
+      propertie1: 0,
+      propertie2: ''
+    };
+  }
+
+  $scope.rules = [{
+    ruleName: 'Default',
+    propertie1: 0x342,
+    propertie2: 'all'
+  }];
+
+  $scope.tmpRule = generateEmptyRule(); // Used for adding rules feature
+
+  $scope.addNewRule = function () {
+    $scope.rules.push($scope.tmpRule);
+    $scope.tmpRule = generateEmptyRule()
+  }
+
   console.log('controller');
 });
